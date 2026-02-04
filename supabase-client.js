94e0659586f29a8c65f7d0784420fbcde3105c53
DeadManDaed <160432445+DeadManDaed@users.noreply.github.com>
2026-02-04 20:46:23 +0100

    Update supabase-client.js

--- supabase-client.js
+++ supabase-client.js
@@ -1,6 +1,6 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
+import { createClient } from 'https://ccjidfxcctmqgpbftiga.supabase.co'
 
 const supabaseUrl = 'https://VOTRE_PROJECT_ID.supabase.co'
-const supabaseKey = 'VOTRE_ANON_KEY'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNjamlkZnhjY3RtcWdwYmZ0aWdhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAwNjc5NTUsImV4cCI6MjA4NTY0Mzk1NX0.1QQUN-5wjCSuwDeFTdJN0XlzOxnxH7h05_9P0vcizPA'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)

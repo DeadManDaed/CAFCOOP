35548d699d2f5407b8ce9d5247d3d01c69644731
DeadManDaed <160432445+DeadManDaed@users.noreply.github.com>
2026-02-05 12:10:41 +0100

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,15 @@
+// pages/_app.js
+import { useEffect } from 'react'
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/sw.js').catch(console.error)
+    }
+  }, [])
+
+  return <Component {...pageProps} />
+}
+
+export default MyApp

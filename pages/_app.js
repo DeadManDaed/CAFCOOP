3f9032038848d4097937a561a95149a7d99013b5
DeadManDaed <160432445+DeadManDaed@users.noreply.github.com>
2026-02-06 11:51:30 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,13 @@
 // C'est cette ligne qui fait la magie !
-import '../styles/globals.css' 
+import '../styles/main.css' 
 import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
     // Enregistrement du Service Worker pour le mode Hors-ligne (PWA)
-    if ('serviceWorker' in navigator) {
+    /*if ('serviceWorker' in navigator) {
       navigator.serviceWorker.register('/sw.js').catch(err => console.log('SW fail', err))
-    }
+    }*/
   }, [])
 
   return <Component {...pageProps} />

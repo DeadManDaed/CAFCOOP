94d8e70631a4a6337270f50079dcccfc78cb7712
DeadManDaed <160432445+DeadManDaed@users.noreply.github.com>
2026-02-06 17:01:04 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,31 @@
-// C'est cette ligne qui fait la magie !
-import '../styles/main.css' 
-import { useEffect } from 'react'
+//pages/_app.js
+import '../styles/main.css' // Importation du CSS que nous avons réparé
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    // Enregistrement du Service Worker pour le mode Hors-ligne (PWA)
-    /*if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/sw.js').catch(err => console.log('SW fail', err))
-    }*/
-  }, [])
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0, viewport-fit=cover" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <title>CAFCOOP App</title>
+      </Head>
 
-  return <Component {...pageProps} />
+      {/* Ici, nous pourrons plus tard envelopper Component avec 
+          <AuthProvider> ou <NotificationProvider> pour tes notifications style WhatsApp 
+      */}
+      <Component {...pageProps} />
+
+      <style jsx global>{`
+        /* Optimisations pour le mode App Mobile */
+        html, body {
+          overscroll-behavior-y: contain; /* Empêche le "pull-to-refresh" natif du navigateur */
+          -webkit-tap-highlight-color: transparent;
+        }
+      `}</style>
+    </>
+  )
 }
 
 export default MyApp

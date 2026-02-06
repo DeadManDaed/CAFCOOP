fa79ad13378e52f856f4a2f00bf7abf70b6339d6
DeadManDaed <160432445+DeadManDaed@users.noreply.github.com>
2026-02-06 22:44:44 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 //pages/_app.js
-import '../styles/main.css' // Importation du CSS que nous avons réparé
+//import '../styles/main.css' // Importation du CSS que nous avons réparé
 import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {

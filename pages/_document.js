1e61d6495c11cdb289a8ff2df3225604ff4d6e82
DeadManDaed <160432445+DeadManDaed@users.noreply.github.com>
2026-02-06 22:44:17 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,7 @@ export default function Document() {
         <meta name="description" content="CAFCOOP - Application agricole du Cameroun" />
         <meta name="theme-color" content="#2E7D32" />
         <link rel="manifest" href="/manifest.json" />
+<link rel="stylesheet" href="/css/main.css" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>

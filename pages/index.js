3e3233c2f9bd1b4f167cd8d1407517edc3f17810
DeadManDaed <160432445+DeadManDaed@users.noreply.github.com>
2026-02-06 18:56:00 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
-import { getSupabase, formatDate, getCurrentUser } from '../utils/supabase-client';
-import { REGIONS_CAMEROUN, BASE_PATHOLOGIES, PRODUITS_AGRICOLES } from '../cafcoop_data';
+import { getSupabase, formatDate, getCurrentUser } from '../lib/supabase-client';
+import { REGIONS_CAMEROUN, BASE_PATHOLOGIES, PRODUITS_AGRICOLES } from 'js/cafcoop_data';
 
 export default function Home() {
   // --- ETATS ---

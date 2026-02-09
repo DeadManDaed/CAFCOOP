9a92f2b6e98a96d19039afcb416c8a8120025ea2
DeadManDaed <160432445+DeadManDaed@users.noreply.github.com>
2026-02-09 13:03:00 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 //pages/_app.js
+/*
 import '../styles/globals.css' // Importation du CSS que nous avons réparé
 import Head from 'next/head'
 
@@ -29,3 +30,30 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp
+*/
+// pages/_app.js
+import { useEffect } from 'react';
+import Head from 'next/head';
+// On garde l'import pour le build, mais on va le "doubler" dans le Head
+import '../styles/globals.css'; 
+
+function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/sw.js')
+        .catch(err => console.log('SW fail', err));
+    }
+  }, []);
+
+  return (
+    <>
+      <Head>
+        {/* On ajoute cette ligne : le "?v=1.0.1" force le navigateur à ignorer le vieux cache */}
+        <link rel="stylesheet" href="/_next/static/css/styles.css?v=1.0.1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+export default MyApp;
+

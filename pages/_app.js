36849e38d718b136acfc2582a81d19b87d117558
DeadManDaed <160432445+DeadManDaed@users.noreply.github.com>
2026-02-09 13:17:23 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,39 +1,3 @@
-//pages/_app.js
-/*
-import '../styles/globals.css' // Importation du CSS que nous avons réparé
-import Head from 'next/head'
-
-function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0, viewport-fit=cover" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-        <title>CAFCOOP App</title>
-      </Head>
-
-      {/* Ici, nous pourrons plus tard envelopper Component avec 
-          <AuthProvider> ou <NotificationProvider> pour tes notifications style WhatsApp 
-      */}
-      <Component {...pageProps} />
-
-      <style jsx global>{`
-   // Optimisations pour le mode App Mobile 
-/*        html, body {
-          overscroll-behavior-y: contain; /* Empêche le "pull-to-refresh" natif du navigateur */
-          -webkit-tap-highlight-color: transparent;
-        }
-      `}</style>
-    </>
-  )
-}
-
-export default MyApp
-
-*/
-
-
 // pages/_app.js
 import { useEffect } from 'react';
 import Head from 'next/head';
